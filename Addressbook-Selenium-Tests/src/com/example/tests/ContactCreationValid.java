16a2b289682ca7e378b446a1bdaac409b11e880b
smalevanny <bbserg@yandex.ru>
2014-03-29 16:35:04 +0400

    Solutions for tasks 11, 12, 13, 14 added

--- Addressbook-Selenium-Tests/src/com/example/tests/ContactCreationValid.java
+++ Addressbook-Selenium-Tests/src/com/example/tests/ContactCreationValid.java
@@ -3,13 +3,26 @@ package com.example.tests;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.example.utils.SortedListOf;
+import static com.example.tests.ContactDataGenerator.loadContactsFromCSVFile;
+import static com.example.tests.ContactDataGenerator.loadContactsFromXMLFile;
 
 public class ContactCreationValid extends TestBase {
-  @Test(dataProvider = "randomValidContactGenerator")
-  public void testContactCreation(ContactData contact) throws Exception {
+	
+	@DataProvider
+	public Iterator<Object[]> contactsFromFile() throws IOException {
+	return wrapContactDataForDataProvider(loadContactsFromXMLFile(new File ("contacts.xml"))).iterator();
+	}
+	
+	@Test(dataProvider = "contactsFromFile")
+	public void testContactCreation(ContactData contact) throws Exception {
     
     //save old list
 	SortedListOf<ContactData> oldList = app.getContactHelper().getContacts(); 
